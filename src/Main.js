16dacf43b21e0afbf48d6be1dcce72f30b6bef2c
haeyong9701 <haeyong9701@gmail.com>
2024-01-11 00:46:43 +0900

    feat: Link로 이동하기

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,8 @@ import HomePage from "./pages/HomePage";
 import CourseListPage from "./pages/CourseListPage";
 import CoursePage from "./pages/CoursePage";
 import WishlistPage from "./pages/WishlistPage";
+import QuestionListPage from "./pages/QuestionListPage";
+import QuestionPage from "./pages/QuestionPage";
 
 function Main() {
   return (
@@ -13,6 +15,8 @@ function Main() {
           <Route path="/" element={<HomePage />} />
           <Route path="courses" element={<CourseListPage />} />
           <Route path="courses/react-frontend-development" element={<CoursePage />} />
+          <Route path="questions" element={<QuestionListPage />} />
+          <Route path="questions/616825" element={<QuestionPage />} />
           <Route path="wishlist" element={<WishlistPage />} />
         </Routes>
       </App>
